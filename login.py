b6b2e3d7416e3aab93b60cda9f5c9f85e87758fb
arjunsohal12 <38443000+arjunsohal12@users.noreply.github.com>
2022-11-06 03:02:51 -0500

    Update login.py

--- login.py
+++ login.py
@@ -36,6 +36,51 @@ def account_verify():
 
     return json.dumps({"email": email, "user_exists": User_Exists, "password": Password_Correct, "id": id})
 
+@app.route('/create_account', methods = ['POST'])
+def create_account():
+    filename = 'dummy_accounts.csv'
+    request_data = request.get_json()
+    email = request_data['email']
+    password = request_data['password']
+    
+    account_info = []
+    account_exists = False
+    key = random.randint(0, 9999)
+    
+    keylist = []
+    with open (filename) as f:
+        reader = csv.reader(f, delimiter=',')
+        for line in reader:
+            if line != [''] or line != ['\n']:
+                for item in line:
+                    item = item.strip()
+                if line != [''] and line !=['\n'] and line != []:
+                    print(line)
+                    keylist.append(line[-1])
+                if email in line:
+                    account_exists = True
+    while key in keylist:
+        key = random.randint(0, 9999)
+    if account_exists == False:
+        account_info.append(email)
+        account_info.append(password)
+        account_info.append([])
+        account_info.append(key)
+    
+        
+        with open(filename, 'a') as f_object:
+         
+            # Pass this file object to csv.writer()
+            # and get a writer object
+            writer_object = writer(f_object)
+         
+            # Pass the list as an argument into
+            # the writerow()
+            writer_object = csv.writer(f_object, lineterminator='\n')
+            writer_object.writerow(account_info)
+         
+            # Close the file object
+            f_object.close()
 
 if __name__ == "__main__":
     app.run(debug=True, port=3001)
